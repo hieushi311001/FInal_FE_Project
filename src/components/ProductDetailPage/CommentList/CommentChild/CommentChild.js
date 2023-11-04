d19618a893fd9e54781b6dce876207c86f7a469f
hieushi311001 <85148961+hieushi311001@users.noreply.github.com>
2023-11-04 16:23:11 +0700

    CommentChild
    HCMUTE

---
+++ src/components/ProductDetailPage/CommentList/CommentChild/CommentChild.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { makeRequest } from "~/services";
+function CommentChild({ id, productId, color }) {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    // Define the API endpoint you want to call
+    const APIdata = {
+      filter: {
+        productId: productId,
+        productColor: color,
+        replyOn: 1, // sử dụng khi cần lấy danh sách bình luận của reply của 1 bình luận nào có id là 1
+      },
+      pagination: {
+        page: 1,
+        limit: 5,
+      },
+    };
+    console.log("test nè: ", productId, "và: ", color, "và: ", id);
+  }, [id, color, productId]);
+  return (
+    <div>
+      {comments.map((comment) => (
+        <div className="d-flex flex-start mt-2">
+          <a className="me-3" href={{}}>
+            <img
+              className="rounded-circle shadow"
+              src={`https://drive.google.com/uc?export=view&id=${comment.avatar}`}
+              alt="avatar"
+              width={65}
+              height={65}
+            />
+          </a>
+          <div className="flex-grow-1 flex-shrink-1 my-custom-margin">
+            <div>
+              <div className="d-flex justify-content-between align-items-center">
+                <p className="mb-1">
+                  {comment.name}
+                  <span className="small-1">- {comment.commentDate}</span>
+                </p>
+              </div>
+              <p className="rounded-box small mb-0">{comment.commentContent}</p>
+            </div>
+            <div className="bg-white">
+              <div className="d-flex flex-row fs-12">
+                <div className="like p-2 cursor">
+                  <i className="fa fa-thumbs-o-up" />
+                  <span className="ml-1">Like</span>
+                </div>
+                <div className="like p-2 cursor">
+                  <i className="fa fa-commenting-o" />
+                  <span className="ml-1">Comment</span>
+                </div>
+                <div className="like p-2 cursor">
+                  <i className="fa fa-share" />
+                  <span className="ml-1">Share</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CommentChild;
