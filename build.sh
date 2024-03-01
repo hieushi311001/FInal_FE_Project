aea75882977a178881179c8335313f36a43db22f
Cao Đinh Sỹ Vỹ <caodinhsyvy@Vys-Mac.local>
2024-03-01 15:23:13 +0000

    update

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ git checkout build
 git add .
 git commit -m 'update'
 git push origin build 
-ssh root@14.225.254.87 'cd FInal_FE_Project && git pull origin build && docker rm -f fe-container && docker compose up -d --build'
+ssh root@103.200.20.153 'cd FInal_FE_Project && git pull origin build && docker rm -f fe-container && docker compose up -d --build'
